9953939a80d3671dd1ed6f5280bd331b97ba68ff
ckagiri <ckagiri@foursquare.com>
2023-01-29 19:06:30 +0000

    configureAdminRouterFromChildren

--- src/core/auth/index.ts
+++ src/core/auth/index.ts
@@ -1,11 +1,15 @@
 import AuthContext from "./AuthContext";
+import useAuthState from './useAuthState';
 import convertAuthProviderFn from "./convertAuthProviderFn";
 import usePermissions from './usePermissions';
 import useLogout from './useLogout';
+
+export * from './useCheckAuth';
 
 export {
   AuthContext,
   convertAuthProviderFn,
   useLogout,
   usePermissions,
+  useAuthState,
 }
